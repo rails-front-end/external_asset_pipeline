1b2dd1bed4fc79310a212c058c29e6b25fce1b19
Richard Macklin <richard.github@nrm.com>
2018-11-15 11:38:33 -0800

    Add WebpackAssetPipeline
    This allows our view helpers like `javascript_include_tag`,
    `asset_path`, and `asset_url` to resolve webpack-compiled assets, so
    that we don't have to change our use of those helpers everywhere.
    
    This code was inspired by
    https://github.com/rails/webpacker/blob/v3.5.5/lib/webpacker/manifest.rb

---
+++ assets.rb
@@ -0,0 +1,47 @@
+# config/initializers/assets.rb
+
+module WebpackAssetPipeline
+  mattr_accessor :manifest
+
+  class Manifest
+    def find(name)
+      data[name.to_s].presence
+    end
+
+    private
+
+    def data
+      if Rails.application.config.cache_revisioned_asset_manifest
+        @data ||= load
+      else
+        load
+      end
+    end
+
+    def load
+      manifest_file_path = Rails.root.join('public', 'packs', 'revisioned-asset-manifest.json')
+      JSON.parse(manifest_file_path.read)
+    end
+  end
+
+  def path_to_asset(source, options = {})
+    value_in_webpack_manifest =
+      WebpackAssetPipeline.manifest.find(pack_name_with_extension(source, options))
+
+    super(
+      value_in_webpack_manifest.present? ? "/packs/#{value_in_webpack_manifest}" : source,
+      options.merge(skip_pipeline: value_in_webpack_manifest.present?)
+    )
+  end
+  alias asset_path path_to_asset
+
+  private
+
+  def pack_name_with_extension(name, options)
+    "#{name}#{compute_asset_extname(name, options)}"
+  end
+end
+
+WebpackAssetPipeline.manifest = WebpackAssetPipeline::Manifest.new
+
+ActionView::Base.include WebpackAssetPipeline
