3e8e64772541a7f6d0fe5fea931eb8d0b84d7f2f
Richard Macklin <richard.github@nrm.com>
2019-03-27 20:04:13 -0700

    Raise in ExternalAssetPipeline#compute_asset_path for missing assets
    This mirrors the behavior of Sprockets::Rails::Helper#compute_asset_path
    https://github.com/rails/sprockets-rails/blob/v3.2.1/lib/sprockets/rails/helper.rb#L83

--- assets.rb
+++ assets.rb
@@ -1,6 +1,8 @@
 # config/initializers/assets.rb
 
 module ExternalAssetPipeline
+  class AssetNotFound < StandardError; end
+
   mattr_accessor :manifest
 
   class Manifest
@@ -27,13 +29,14 @@ module ExternalAssetPipeline
   # Overrides the built-in `ActionView::Helpers::AssetUrlHelper#compute_asset_path` to use the
   # external asset pipeline, in the same manner that sprockets-rails does:
   # https://github.com/rails/sprockets-rails/blob/v3.2.1/lib/sprockets/rails/helper.rb#L74-L96
-  def compute_asset_path(source, options = {})
+  def compute_asset_path(source, _options = {})
     value_in_asset_manifest = ExternalAssetPipeline.manifest.find(source)
 
     if value_in_asset_manifest
       "/packs/#{value_in_asset_manifest}"
     else
-      super(source, options)
+      raise AssetNotFound,
+            "The asset #{source.inspect} is not present in the asset manifest"
     end
   end
 end
