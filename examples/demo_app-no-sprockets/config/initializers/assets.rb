fdeed7656a86d4076c4ab6eecc165b9be6c8488e
Richard Macklin <richard.github@nrm.com>
2019-03-30 16:09:57 -0700

    [demo_app-no-sprockets] Make sass helper resolve to gulp-compiled images
    This means that sprockets is no longer needed for image compilation.

--- examples/demo_app-no-sprockets/config/initializers/assets.rb
+++ examples/demo_app-no-sprockets/config/initializers/assets.rb
@@ -12,3 +12,30 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+module ExternalAssetPipelineExtension
+  class SassProcessor
+    def self.asset_path(path, _options = {})
+      asset = ExternalAssetPipeline.manifest.find(JSON.parse(path.to_s))
+
+      if asset
+        Sass::Script::String.new("#{asset}", :string)
+      else
+        raise "Unrecognized sass_asset_path path argument: #{path}"
+      end
+    end
+  end
+end
+
+# Temporarily patch the Sprockets SassProcessor to be able to resolve `img-url`
+# function parameters to gulp-compiled image paths. This will go away once
+# stylesheets are also processed outside of sprockets
+module Sprockets
+  class SassProcessor
+    module Functions
+      def asset_path(path, options = {})
+        ExternalAssetPipelineExtension::SassProcessor.asset_path(path, options)
+      end
+    end
+  end
+end
