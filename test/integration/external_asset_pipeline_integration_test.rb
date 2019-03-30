42bf099aedc7bfb81a74043607129b860da0c04c
Richard Macklin <richard.github@nrm.com>
2019-03-30 16:51:19 -0700

    Make the integration test also cover `stylesheet_link_tag` output
    Importantly, this verifies that the demo_app-no-sprockets example app
    uses the ExternalAssetPipeline for all assets, rather than just for
    javascript assets (while the demo_app example app lets sprockets handle
    other assets).

--- test/integration/external_asset_pipeline_integration_test.rb
+++ test/integration/external_asset_pipeline_integration_test.rb
@@ -15,5 +15,19 @@ class ExternalAssetPipelineIntegrationTest < ActionDispatch::IntegrationTest
     assert_select 'script' do |elements|
       assert_equal script_path, elements[0][:src]
     end
+
+    assert_select 'link' do |elements|
+      assert_match stylesheet_href_matcher, elements[0][:href]
+    end
+  end
+
+  private
+
+  def stylesheet_href_matcher
+    if Rails.root.to_s.include?('demo_app-no-sprockets')
+      %r[^/packs/application-.{10}\.css$]
+    else
+      %r[^/assets/application-.{64}\.css$]
+    end
   end
 end
