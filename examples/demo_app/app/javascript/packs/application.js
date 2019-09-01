670e631918f86d6082c72e4a1d4d2c360d7b8fe9
Richard Macklin <richard.github@nrm.com>
2019-09-01 13:08:32 +0200

    [demo_app] Remove generated ActionCable javascript files

--- examples/demo_app/app/javascript/packs/application.js
+++ examples/demo_app/app/javascript/packs/application.js
@@ -6,7 +6,6 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
-require("channels")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
