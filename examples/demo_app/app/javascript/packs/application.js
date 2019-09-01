a3b86f701dbaf7166fad39d6d90f163c903a2c53
Richard Macklin <richard.github@nrm.com>
2019-09-01 13:01:22 +0200

    Remove demo_app
    We will be replacing demo_app with a freshly generated rails 6 app,
    because there are some differences between a freshly generated rails 6
    app and an app that was upgraded from rails 5.2 to rails 6.

--- examples/demo_app/app/javascript/packs/application.js
+++
@@ -1,7 +0,0 @@
-import * as ActiveStorage from '@rails/activestorage';
-import Rails from '@rails/ujs';
-import Turbolinks from 'turbolinks';
-
-Rails.start();
-ActiveStorage.start();
-Turbolinks.start();
