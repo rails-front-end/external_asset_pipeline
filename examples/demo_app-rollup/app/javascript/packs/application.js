2fa8412bd9b8bb2a72f80becb2747d726e0cd05f
Richard Macklin <richard.github@nrm.com>
2019-09-02 19:09:31 +0200

    [demo_app-rollup] Apply changes from freshly generated rails 6 app
    This is 82130508a008b85ba6b3b966ffc7a5af5e7f8c7a but for the
    demo_app-rollup app, and without upgrades to the package.json
    devDependencies.

--- examples/demo_app-rollup/app/javascript/packs/application.js
+++ examples/demo_app-rollup/app/javascript/packs/application.js
@@ -2,5 +2,5 @@ import * as ActiveStorage from '@rails/activestorage';
 import '@rails/ujs';
 import Turbolinks from 'turbolinks';
 
-ActiveStorage.start();
 Turbolinks.start();
+ActiveStorage.start();
