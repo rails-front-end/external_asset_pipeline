82130508a008b85ba6b3b966ffc7a5af5e7f8c7a
rmacklin <1863540+rmacklin@users.noreply.github.com>
2019-09-02 11:45:04 +0200

    Apply changes to demo_app that would be generated in a freshly created rails 6.0.0 application (#9)

--- examples/demo_app/db/schema.rb
+++ examples/demo_app/db/schema.rb
@@ -10,6 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_01_193936) do
+ActiveRecord::Schema.define(version: 0) do
 
 end
