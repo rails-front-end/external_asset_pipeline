a009c5bdb7409ac16af51e0e093b2c751f03998e
Richard Macklin <richard.github@nrm.com>
2019-09-03 18:30:23 +0200

    [demo_app-brunch] Remove lib/tasks/assets

--- examples/demo_app-brunch/lib/tasks/assets/build.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const buildStylesTask = require('./build-styles');
-
-const build = gulp.parallel(buildStylesTask);
-
-build.description = 'Build all static assets';
-
-module.exports = build;
