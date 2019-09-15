d349eeb9caed586c7c2e29e871137552da594e10
Richard Macklin <richard.github@nrm.com>
2019-09-15 12:34:56 +0300

    Abstract the manifest singleton behind external_asset_pipeline_manifest
    The `external_asset_pipeline_manifest` helper method can be overridden
    to return a different `Manifest` instance than the default value, e.g.
    to use multiple distinct `Manifest` instances in different rails engines

--- lib/external_asset_pipeline/helper.rb
+++ lib/external_asset_pipeline/helper.rb
@@ -4,12 +4,18 @@ require 'external_asset_pipeline'
 
 module ExternalAssetPipeline
   module Helper
+    # This helper method can be overridden to return a different Manifest
+    # instance, e.g. to have distinct instances in different rails engines.
+    def external_asset_pipeline_manifest
+      ExternalAssetPipeline.manifest
+    end
+
     # Overrides the built-in
     # `ActionView::Helpers::AssetUrlHelper#compute_asset_path` to use the
     # external asset pipeline, in the same manner that sprockets-rails does:
     # https://github.com/rails/sprockets-rails/blob/v3.2.1/lib/sprockets/rails/helper.rb#L74-L96
     def compute_asset_path(source, options = {})
-      manifest = ExternalAssetPipeline.manifest
+      manifest = external_asset_pipeline_manifest
       asset = manifest.find(source)
 
       options[:host] = asset[:host] if asset && asset[:host]
