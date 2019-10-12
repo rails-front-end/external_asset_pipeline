f03428054057e4426c371330334ccaac5dd20dcc
Richard Macklin <richard.github@nrm.com>
2019-10-12 09:59:26 -0700

    Add `VERSION::MAJOR`, `VERSION::MINOR`, and `VERSION::PATCH` constants
    This allows for more fine-grained programmatic access (for example, you
    can now write comparisons involving just
    `ExternalAssetPipeline::VERSION::MAJOR`, instead of having to parse it
    out of `ExternalAssetPipeline::VERSION::STRING`.

--- lib/external_asset_pipeline/version.rb
+++ lib/external_asset_pipeline/version.rb
@@ -2,6 +2,10 @@
 
 module ExternalAssetPipeline
   module VERSION
+    MAJOR = 0
+    MINOR = 6
+    PATCH = 0
+
     STRING = '0.6.0'
   end
 end
--- test/external_asset_pipeline_test.rb
+++ test/external_asset_pipeline_test.rb
@@ -4,6 +4,13 @@ require 'test_helper'
 
 class ExternalAssetPipelineTest < Minitest::Test
   def test_that_it_has_a_version_number
+    refute_nil ::ExternalAssetPipeline::VERSION::MAJOR
+    assert_kind_of Integer, ::ExternalAssetPipeline::VERSION::MAJOR
+    refute_nil ::ExternalAssetPipeline::VERSION::MINOR
+    assert_kind_of Integer, ::ExternalAssetPipeline::VERSION::MINOR
+    refute_nil ::ExternalAssetPipeline::VERSION::PATCH
+    assert_kind_of Integer, ::ExternalAssetPipeline::VERSION::PATCH
+
     refute_nil ::ExternalAssetPipeline::VERSION::STRING
     assert_kind_of String, ::ExternalAssetPipeline::VERSION::STRING
   end
