2b96d63011e437234f781e5366c5db04409fd8ae
Richard Macklin <richard.github@nrm.com>
2021-04-17 09:22:37 -0700

    [demo_app] Apply changes after running `bin/rails app:update`

--- examples/demo_app/config/application.rb
+++ examples/demo_app/config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 require 'external_asset_pipeline/railtie'
 
@@ -15,9 +15,12 @@ module DemoApp
 
     config.external_asset_pipeline.fall_back_to_sprockets = true
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
