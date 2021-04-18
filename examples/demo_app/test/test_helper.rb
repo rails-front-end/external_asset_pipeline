c4235d1283e370b4327f0ae76ea4cda2db05192f
Richard Macklin <richard.github@nrm.com>
2021-04-18 14:30:30 -0700

    [demo_app] Disable parallel tests
    This is just a demo app, so we don't need to parallelize tests.

--- examples/demo_app/test/test_helper.rb
+++ examples/demo_app/test/test_helper.rb
@@ -4,7 +4,7 @@ require "rails/test_help"
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  # parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
