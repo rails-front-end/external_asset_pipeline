be6ab257beffd1f2dc48b139e72922fae2e19442
Richard Macklin <richard.github@nrm.com>
2021-04-18 14:15:59 -0700

    [demo_app] Reformat application.js to match original style
    This keeps the demo app's application.js consistent with all the others.

--- examples/demo_app/app/javascript/packs/application.js
+++ examples/demo_app/app/javascript/packs/application.js
@@ -1,7 +1,7 @@
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
+import * as ActiveStorage from '@rails/activestorage';
+import Rails from '@rails/ujs';
+import Turbolinks from 'turbolinks';
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
