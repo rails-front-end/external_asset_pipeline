339ff95f577db941f56ae9f324a4cd8abb03f7db
Richard Macklin <richard.github@nrm.com>
2021-04-18 14:29:05 -0700

    [demo_app] Integrate external_asset_pipeline railtie
    The railtie is required after `rails/all` which includes
    `sprockets/railtie`. This ensures that the ExternalAssetPipeline takes
    priority over sprockets (but falls back to sprockets when assets are not
    found in the ExternalAssetPipeline, as indicated by enabling the
    `config.external_asset_pipeline.fall_back_to_sprockets` setting).
    
    Additionally, we disable manifest caching in the development environment
    so that the server won't need to be restarted to pick up changes in the
    manifest during development.

--- examples/demo_app/config/application.rb
+++ examples/demo_app/config/application.rb
@@ -2,6 +2,8 @@ require_relative "boot"
 
 require "rails/all"
 
+require "external_asset_pipeline/railtie"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -10,6 +12,8 @@ module DemoApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+
+    config.external_asset_pipeline.fall_back_to_sprockets = true
 
     # Configuration for the application, engines, and railties goes here.
     #
