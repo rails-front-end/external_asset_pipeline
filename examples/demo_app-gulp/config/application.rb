67c5b73435304e2dc33b7b6e1d7a6389fcf9e769
Richard Macklin <richard.github@nrm.com>
2021-04-19 19:48:01 -0700

    [demo_app-gulp] Apply changes from freshly generated rails 6.1 app
    This is 72248706fce0788656c67068093c1e9028a7306e but for the
    demo_app-gulp app.

--- examples/demo_app-gulp/config/application.rb
+++ examples/demo_app-gulp/config/application.rb
@@ -14,7 +14,7 @@ require "action_view/railtie"
 require "action_cable/engine"
 require "rails/test_unit/railtie"
 
-require 'external_asset_pipeline/railtie'
+require "external_asset_pipeline/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
